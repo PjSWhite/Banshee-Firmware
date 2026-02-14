e6aed57843932de64032e2085c4c09a6816dba67
Kiwifuit <75169058+Kiwifuit@users.noreply.github.com>
2026-02-14 09:18:31 +0800

    feat: usb device

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,19 @@
 use hal::fugit::ExtU32;
 use panic_halt as _;
 use rp2040_hal::{self as hal};
+use usb_device::{
+    bus::UsbBusAllocator,
+    device::{StringDescriptors, UsbDeviceBuilder, UsbVidPid},
+};
+use usbd_serial::SerialPort;
 
 #[unsafe(link_section = ".boot2")]
 #[used]
 pub static BOOT2: [u8; 256] = rp2040_boot2::BOOT_LOADER_W25Q080;
 
+// mod cmd;
 mod status;
+mod usb;
 
 #[hal::entry]
 fn main() -> ! {
@@ -39,8 +46,57 @@ fn main() -> ! {
     let led_pin = pins.gpio25.into_push_pull_output();
     let mut status = status::StatusPin::new(led_pin, timers.alarm_0().unwrap());
 
+    let usb_bus = UsbBusAllocator::new(hal::usb::UsbBus::new(
+        pac.USBCTRL_REGS,
+        pac.USBCTRL_DPRAM,
+        clocks.usb_clock,
+        true,
+        &mut pac.RESETS,
+    ));
+
+    let mut usb = usb::UsbCommandLine::new(&usb_bus);
+
+    // let usb_bus = UsbBusAllocator::new(hal::usb::UsbBus::new(
+    //     pac.USBCTRL_REGS,
+    //     pac.USBCTRL_DPRAM,
+    //     clocks.usb_clock,
+    //     true,
+    //     &mut pac.RESETS,
+    // ));
+
+    // let mut serial = SerialPort::new(&usb_bus);
+
+    // // NOTE: https://pid.codes/howto/
+    // //       We should make our own PID
+    // let mut dev = UsbDeviceBuilder::new(&usb_bus, UsbVidPid(0x2e8a, 0x000a))
+    //     .strings(&[StringDescriptors::default()
+    //         .manufacturer("CIT-U SHS SY 2526 STEM-12 ALTRUISM RESEARCH 4 GROUP 3")
+    //         .product("OpenPanahon Station Node")
+    //         .serial_number("NULL")])
+    //     .unwrap()
+    //     .device_class(2)
+    //     .build();
+
     watchdog.start(500.millis());
     loop {
+        usb.poll();
+
+        // if dev.poll(&mut [&mut serial]) {
+        //     let mut buf = [0; 32];
+
+        //     match serial.read(&mut buf) {
+        //         Err(_) | Ok(0) => (),
+        //         Ok(count) => {
+        //             buf.iter_mut()
+        //                 .take(count)
+        //                 .for_each(u8::make_ascii_uppercase);
+
+        //             let reply_buffer = &buf[..count];
+        //             serial.write(reply_buffer).unwrap();
+        //         }
+        //     }
+        // }
+
         status.in_loop();
 
         watchdog.feed();
---
+++ src/usb.rs
@@ -0,0 +1,46 @@
+use rp2040_hal as hal;
+use usb_device::{
+    bus::UsbBusAllocator,
+    device::{StringDescriptors, UsbDevice, UsbDeviceBuilder, UsbVidPid},
+};
+use usbd_serial::SerialPort;
+
+pub struct UsbCommandLine<'a> {
+    pub cmdline: SerialPort<'a, hal::usb::UsbBus>,
+    pub logger: SerialPort<'a, hal::usb::UsbBus>,
+    device: UsbDevice<'a, hal::usb::UsbBus>,
+    // allocator: UsbBusAllocator<hal::usb::UsbBus>,
+}
+
+impl<'a> UsbCommandLine<'a> {
+    pub fn new(allocator: &'a UsbBusAllocator<hal::usb::UsbBus>) -> Self {
+        let cmdline = SerialPort::new_with_interface_names(
+            allocator,
+            Some("OpenPanahon Commandline"),
+            Some("cmd"),
+        );
+        let logger =
+            SerialPort::new_with_interface_names(allocator, Some("OpenPanahon Log"), Some("log"));
+
+        // NOTE: https://pid.codes/howto/
+        //       We should make our own PID
+        let device = UsbDeviceBuilder::new(allocator, UsbVidPid(0x2e8a, 0x000a))
+            .strings(&[StringDescriptors::default()
+                .manufacturer("CIT-U SHS SY 2526 STEM-12 ALTRUISM RESEARCH 4 GROUP 3")
+                .product("OpenPanahon Station Node")
+                .serial_number("NULL")])
+            .unwrap()
+            .device_class(2)
+            .build();
+
+        Self {
+            cmdline,
+            logger,
+            device,
+        }
+    }
+
+    pub fn poll(&mut self) -> bool {
+        self.device.poll(&mut [&mut self.cmdline, &mut self.logger])
+    }
+}
